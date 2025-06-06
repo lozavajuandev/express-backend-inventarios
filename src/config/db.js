f3c272ccd417ffe7d08fb953b9cab42caa6a2113
Juan Jose <patriciavargaspena@Air-de-Juan-Jose.local>
2025-06-06 07:44:26 -0500

    db Conection fixed

--- src/config/db.js
+++ src/config/db.js
@@ -5,12 +5,23 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const dbConnection = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+let dbConnection;
+
+if (process.env.NODE_ENV === "production") {
+  dbConnection = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  dbConnection = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  });
+}
 
 export default dbConnection;
